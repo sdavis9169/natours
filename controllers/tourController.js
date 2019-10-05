39114a11291b24428b615433b40c34f6afdba4a2
Sdavis <stevendavis0622@gmail.com>
2019-10-04 22:13:34 -0600

    added getAll query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,7 +16,19 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const queryStr = JSON.stringify(queryObj);
+    queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(queryObj);
+
+    //EXECUTE QUERY
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
