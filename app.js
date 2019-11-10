521babd14730b393d7564d1244b359ae99f9e208
Sdavis <stevendavis0622@gmail.com>
2019-11-10 03:33:55 -0700

    linked css and started html

--- app.js
+++ app.js
@@ -42,7 +42,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Steven'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
