aa4a86f94f014f757da4f2b52a8004ec172cd7da
Sdavis <stevendavis0622@gmail.com>
2019-11-21 13:01:41 -0700

    what happend

--- routes/viewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const viewsController = require('../controllers/viewsController');
-
-const router = express.Router();
-
-router.get('/overview', viewsController.getOverview);
-router.get('/tour/:slug', viewsController.getTour);
-
-module.exports = router;
