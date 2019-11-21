0ea8e61a2247a1600e66da40fc51c9dd3837301e
Sdavis <stevendavis0622@gmail.com>
2019-11-20 21:09:28 -0700

    overview page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -41,13 +42,8 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Steven'
-  });
-});
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ controllers/viewsController.js
@@ -0,0 +1,20 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+
+  // 3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours
+  });
+});
+
+exports.getTour = (req, res) => {
+  res.status(200).render('overview', {
+    title: 'The Forest Hiker'
+  });
+};
